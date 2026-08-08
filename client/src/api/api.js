1eec00928c99ad8965768fc0e3145fb893a96519
Zain Bin Ishfaq <zainbinishfaq@gmail.com>
2026-08-08 11:54:03 +0500

    prepare frontend for deployment

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,21 +1,28 @@
-
 import axios from "axios";
 
-const api = axios.create({baseURL: "http://localhost:5000/api"});
-
-api.interceptors.request.use((config)=>{
-    const token = localStorage.getItem("token")
-    if(token) {
-        config.headers.token = token;
-    }
-   return config;
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-api.interceptors.response.use((response)=> response,(error)=>{
-    if(error.response && error.response.status === 401){
-            localStorage.removeItem("token")
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.token = token;
+  }
+
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
     }
+
     return Promise.reject(error);
-} );
+  }
+);
 
 export default api;
